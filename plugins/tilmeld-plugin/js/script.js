9ed1e2125c389273fd2739564ae02852a2cc4839
monicastrandgaard <71007327+monicastrandgaard@users.noreply.github.com>
2021-11-02 19:20:59 +0100

    Update script.js

--- plugins/tilmeld-plugin/js/script.js
+++ plugins/tilmeld-plugin/js/script.js
@@ -1,7 +1,11 @@
-//jQuery.noConflict();
-jQuery(document).ready(function( $ ) {
-  $('.popupCloseButton').on('click', function() {
-    $('.login-form').hide();
-  })
-	
-});
+/* Når man trykker på krydset, "lukker" vores popup ved, at vi her siger, at den skal "display = none" */
+
+document.getElementById("popupCloseButton").onclick = function() {
+  document.getElementById("login-form").style.display = "none";
+}
+
+/* Her sørger vi for, at vores popup først vises, når man trykker på knappen, som har id'et "popup_button" */
+
+document.getElementById("popup_button").onclick = function() {
+  document.getElementById("login-form").style.display = "block";
+}
