b06ae2fec049d741ba78a1dd36109cf165dcebe0
annesofiepedersen <71007063+annesofiepedersen@users.noreply.github.com>
2021-11-03 13:17:12 +0100

    Update functions.php

--- astra-child-theme/functions.php
+++ astra-child-theme/functions.php
@@ -5,7 +5,7 @@ function my_theme_enqueue_styles() {
     $parenthandle = 'parent-style'; 
     $theme = wp_get_theme();
     wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
-        array(),  // if the parent theme code has a dependency, copy it to here
+        array(), 
         $theme->parent()->get('Version')
     );
     wp_enqueue_style( 'child-style', get_stylesheet_uri(),
