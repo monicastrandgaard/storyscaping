5919b51b19a574555c37a569ca57342d7ee6d9e6
annesofiepedersen <71007063+annesofiepedersen@users.noreply.github.com>
2021-11-03 13:16:58 +0100

    Update functions.php

--- astra-child-theme/functions.php
+++ astra-child-theme/functions.php
@@ -2,7 +2,7 @@
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parenthandle = 'parent-style'; 
     $theme = wp_get_theme();
     wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
         array(),  // if the parent theme code has a dependency, copy it to here
@@ -10,6 +10,6 @@ function my_theme_enqueue_styles() {
     );
     wp_enqueue_style( 'child-style', get_stylesheet_uri(),
         array( $parenthandle ),
-        $theme->get('Version') // this only works if you have Version in the style header
+        $theme->get('Version')
     );
 }
