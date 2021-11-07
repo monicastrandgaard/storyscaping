a88bbaaa7809a5774e7db2c282be25155cba6915
monicastrandgaard <71007327+monicastrandgaard@users.noreply.github.com>
2021-11-07 20:38:52 +0100

    Update functions.php

--- astra-child-theme/functions.php
+++ astra-child-theme/functions.php
@@ -2,14 +2,14 @@
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    $parenthandle = 'parent-style'; 
+    $parenthandle = 'parent-style';
     $theme = wp_get_theme();
     wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
-        array(), 
+        array(),  
         $theme->parent()->get('Version')
     );
     wp_enqueue_style( 'child-style', get_stylesheet_uri(),
         array( $parenthandle ),
-        $theme->get('Version')
+        $theme->get('Version') // this only works if you have Version in the style header
     );
 }
